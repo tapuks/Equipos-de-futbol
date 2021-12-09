f348037c6ab9c8d38fda156279a49777717fd4fe
tapuks <david_berdiell@hotmail.com>
2021-12-09 22:13:51 +0100

    trofeos ok

--- Equipos_futbol_app/views.py
+++ Equipos_futbol_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from Equipos_futbol_app.models import Equipo, Plantilla
+from Equipos_futbol_app.models import Equipo, Plantilla, Trofeos
 
 
 def home(request):
@@ -15,5 +15,19 @@ def home(request):
 def plantilla(request, id_equipo):
     plantilla = Plantilla.objects.filter(equipo_id=id_equipo)
     equipo = Equipo.objects.get(id=id_equipo)
-    return render(request, 'plantilla.html', {'plantilla': plantilla, 'equipo': equipo})
+    trofeos = Trofeos.objects.filter(equipo_id = id_equipo)
+    copa_rey = 0
+    copa_liga = 0
+    copa_uefa = 0
+    copa_europa = 0
+    for trofeo in trofeos:
+        copa_rey = trofeo.copa_rey
+        copa_liga = trofeo.copa_liga
+        copa_uefa = trofeo.copa_uefa
+        copa_europa = trofeo.copa_europa
+
+
+
+    return render(request, 'plantilla.html', {'plantilla': plantilla, 'equipo': equipo, 'trofeos': trofeos, 'range_copa_rey': range(copa_rey),
+    'range_copa_liga': range(copa_liga), 'range_copa_uefa': range(copa_uefa), 'range_copa_europa': range(copa_europa)} )
 
